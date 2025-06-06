cfc67b84caf7b6d153f18db6e7070a735060681f
lydiarenee <plutosquest6@gmail.com>
2025-06-05 21:01:02 -0600

    Added js src script to index.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector(".temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
+  let cityElement = document.querySelector(".city");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
@@ -16,14 +16,14 @@ function searchCity(city) {
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
+  let searchInput = document.querySelector(".search-form-input");
+  let cityElement = document.querySelector(".city");
 
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
